98e8622cf081d8710ac586f3ac945e1c311aabc1
chill31 <89910163+chill31@users.noreply.github.com>
2023-10-02 18:41:25 +0530

    fixed URL mistake for production

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -1,21 +1,22 @@
-import Container from "@/components/Container";
-import QuizCard from "@/components/QuizCard";
-import Title from "@/components/Title";
+import Container from '@/components/Container';
+import QuizCard from '@/components/QuizCard';
 
 export default async function Quiz({ params }: { params: { id: string } }) {
 
-  const res = await fetch('http://localhost:3000/api/data', {
+  const res = await fetch(process.env.URL + '/api/data', {
     method: 'POST',
     cache: 'no-cache',
     body: JSON.stringify({ id: params.id }),
   });
   const recieved = await res.json();
 
+  console.log(recieved); // add this line to check the data being returned
+
   const {quiz, questions} = recieved;
   if(!quiz || !questions) {
     return (
       <Container>
-        <Title>404</Title>
+        <h1>404</h1>
         <p className="text-center mx-4">quiz not found. Ask the person who sent you this quiz id for a new or correct one.</p>
       </Container>
     )
@@ -23,7 +24,7 @@ export default async function Quiz({ params }: { params: { id: string } }) {
 
   return (
     <Container>
-      <Title>{quiz.title}</Title>
+      <h1>{quiz.title}</h1>
       <QuizCard questions={questions} quiz={quiz}></QuizCard>
     </Container>
   )
