ad8e60c0d2d839e9977965d7045cc03ed0e26519
chill31 <89910163+chill31@users.noreply.github.com>
2023-10-02 18:34:45 +0530

    added try catch

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -8,34 +8,41 @@ type ResponseQuestionObject = {
 
 export async function POST(req: Request) {
   const { id } = await req.json();
-  const quiz = await prisma.quiz.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  const foundQuestions = await prisma.question.findMany({
-    where: {
-      quizId: id,
-    },
-  });
 
-  const questions: ResponseQuestionObject[] = [];
+  try {
+    const quiz = await prisma.quiz.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    const foundQuestions = await prisma.question.findMany({
+      where: {
+        quizId: id,
+      },
+    });
 
-  await Promise.all(
-    foundQuestions.map(async (question) => {
-      const foundOptions = await prisma.option.findMany({
-        where: {
+    const questions: ResponseQuestionObject[] = [];
+
+    await Promise.all(
+      foundQuestions.map(async (question) => {
+        const foundOptions = await prisma.option.findMany({
+          where: {
+            question: question,
+          },
+        });
+        questions.push({
           question: question,
-        },
-      });
-      questions.push({
-        question: question,
-        options: foundOptions,
-      });
-    })
-  );
+          options: foundOptions,
+        });
+      })
+    );
 
-  return new Response(JSON.stringify({ quiz, questions }), {
-    status: 200,
-  });
+    return new Response(JSON.stringify({ quiz, questions }), {
+      status: 200,
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ msg: 'Error [400]', error: e }), {status: 400});
+  } finally {
+    prisma.$disconnect();
+  }
 }
